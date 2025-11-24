9e448079da631ed30f8e961d8b2c10f69aca0177
DevStackJunior <pv00wyv@eduvaud.ch>
2025-11-24 11:23:06 +0100

    feat: EdgeJS View | Home, Users
    Create the home page overview according to the template of teachers
    Starting the users page overview

--- flashcards/start/routes.ts
+++ flashcards/start/routes.ts
@@ -1,3 +1,6 @@
+import decks_controller from '#controllers/decks_controller'
+import users_controller from '#controllers/users_controller'
+import { errors } from '@adonisjs/core'
 /*
 |--------------------------------------------------------------------------
 | Routes file
@@ -9,12 +12,28 @@
 
 import router from '@adonisjs/core/services/router'
 
-router.get('/', async () => {
-  return {
-    hello: 'world',
+router.get('/', async ({ view }) => {
+  try {
+    const homePage = await view.render('home')
+    return homePage
+  } catch (error) {
+    if (error instanceof errors.E_ROUTE_NOT_FOUND) {
+      console.log(`${error?.name || 'Row'} not found`)
+    }
+    throw error
   }
 })
 
-router.get('/', async ({ view }) => {
-  return view.render('home')
-})
+router.resource('decks', decks_controller).only(['show', 'create', 'update'])
+
+router.on('/').render('/decks')
+
+router.get('/users', [users_controller, 'create'])
+
+// users to show up on the router
+router.on('/').render('/users/show')
+
+/*router.get('/users/:id', async ({ view, params }) => {
+  // Renders a view and passes dynamic data from the route parameters
+  return view.render('users/show', { userId: params.id })
+})*/
